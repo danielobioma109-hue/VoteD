60ed9eb72c97004dcf591c02f30e58b7ba79a3ec
danielobioma109-hue <danielobioma109@gmail.com>
2026-07-12 03:57:01 +0100

    Add vote button alert on DOMContentLoaded

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const voteButton = document.querySelector("button");
+
+    voteButton.addEventListener("click", function () {
+        alert("Welcome to VoteNexus! The voting page is coming soon.");
+    });
+});
